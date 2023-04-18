086a9908d125ee2eb4a2983ca3425c884303c8a1
unknown <ksec1995@gmail.com>
2023-04-18 14:22:09 +0700

    update path

--- vpn/vpn.go
+++ vpn/vpn.go
@@ -65,7 +65,7 @@ const (
 	TIME_TO_TRY = 5 * time.Second
 	MAX_TRY     = 10
 
-	WEBSOCKET_PATH              = "/tunnel"
+	WEBSOCKET_PATH              = "/home"
 	VERSION_PATH                = "/version"
 	AUTHEN_HEADER               = "User"
 	USERAGENT                   = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.3"
